b9f11e759011adfc86779ed3c1d17faa3301b9a4
Timothy Do <tydo83@gmail.com>
2021-08-17 15:31:44 -0400

    updated usersRouter

--- routes/users/usersRouter.js
+++ routes/users/usersRouter.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var { signUp, login, updateUserPassword } = require('./controller/userController')
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/sign-up', signUp)
+router.post('/login',login )
+router.post('/updated-password', updateUserPassword)
 
 module.exports = router;
